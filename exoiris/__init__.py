85ce382235903f616391a1e4086e7cdbefd9c5ff
Hannu Parviainen <hpparvi@gmail.com>
2024-11-08 08:57:57 +0000

    Fixed copyright formatting.

--- exoiris/__init__.py
+++ exoiris/__init__.py
@@ -1,18 +1,18 @@
 #  ExoIris: fast, flexible, and easy exoplanet transmission spectroscopy in Python.
 #  Copyright (C) 2024 Hannu Parviainen
 #
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from .exoiris import ExoIris, load_model # noqa
 from .tsdata import TSData, TSDataSet # noqa
