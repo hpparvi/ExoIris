ecc8325a383f530949bf988c68c51be155dad7f9
Hannu Parviainen <hannu@iac.es>
2025-08-20 10:41:21 +0100

    Fix setup_multiprocessing.py to work on Macs.

--- doc/source/examples/setup_multiprocessing.py
+++ doc/source/examples/setup_multiprocessing.py
@@ -5,6 +5,13 @@ os.environ["MKL_NUM_THREADS"] =        "1"
 os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] =    "1"
 
+# I'm forcing the multiprocessing start method to 'fork' on Macs
+# because 'spawn' seems to cause errors with the numba code.
+from sys import platform
+if 'darwin' in platform:
+    import multiprocessing as mp
+    mp.set_start_method('fork')
+
 from numba import set_num_threads, config
-config.THREADING_LAYER = 'forksafe'
+config.THREADING_LAYER = 'safe'
 set_num_threads(1)
